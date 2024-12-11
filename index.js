1bd0cb32d864219d4365b39b3082d00996995612
IvanLabza <100775210+IvanLabza@users.noreply.github.com>
2024-12-11 23:19:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,29 +2,39 @@ async function IP_TEST() {
 fetch('https://api.ipify.org?format=json')
 .then(response => response.json())
 .then(data => {
-IP_local(data.ip);
+
+const newDiv = document.createElement('h1');
+
+newDiv.textContent = `${data.ip}`;
+
+newDiv.style.color = 'blue';
+newDiv.style.fontSize = '20px';
+
+document.body.appendChild(newDiv);
+
+
 })
 .catch(error => {
 console.error("Ошибка при получении IP-адреса:", error);
 });
 }
 
-async function IP_local(ip) {
-fetch(`https://ipapi.co/${ip}/json/`)
-.then(response => response.json())
-.then(data => {
-    createText(data);
-    document.querySelector("body").innerHTML(`
-    <ul>
-        <li>${data.city}</li>
-        <li>${data.region}</li>
-        <li>${data.country_name}</li>
-    </ul>
-    `);
+// async function IP_local(ip) {
+// fetch(`https://ipapi.co/${ip}/json/`)
+// .then(response => response.json())
+// .then(data => {
+//     createText(data);
+//     document.querySelector("body").innerHTML(`
+//     <ul>
+//         <li>${data.city}</li>
+//         <li>${data.region}</li>
+//         <li>${data.country_name}</li>
+//     </ul>
+//     `);
 
-});
+// });
 
-}
+// }
 
 IP_TEST();
 
